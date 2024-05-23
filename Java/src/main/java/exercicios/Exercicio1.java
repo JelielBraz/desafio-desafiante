991429a9857e0d5c779a99000c5af0e90e2bf26a
Jeliel Braz <jelielbraz71@gmail.com>
2024-05-22 22:42:29 -0300

    Adicionar early return

--- Java/src/main/java/exercicios/Exercicio1.java
+++ Java/src/main/java/exercicios/Exercicio1.java
@@ -3,9 +3,7 @@ package exercicios;
 /** Faça uma função recursiva que calcule e retorne o fatorial de um número inteiro N.*/
 public class Exercicio1 {
     public static Long fatorial(Long n) {
-        if (n == 0) {
-            return 1L;
-        }
+        if (n == 0) return 1L;
 
         Long resultado = 1L;
         for (Long i=n; i > 0; i--) {
